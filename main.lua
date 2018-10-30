fc544fd44f3927440895a9080b0090b30e4473c7
skyking1993 <44384838+skyking1993@users.noreply.github.com>
2018-10-30 08:30:33 +0800

    new
    hello world

--- main.lua
+++ main.lua
@@ -9,6 +9,11 @@ local msgText = display.newText( "Hello, world!", display.contentCenterX, displa
 msgText:setFillColor( 0.2, 0.6, 0.8 )
 
 
-local msgText = display.newText( "Hello, world!", display.contentCenterX, display.contentWidth + 100, native.systemFont, 32 )
-msgText:setFillColor( 0.7, 0.6, 0.7 )
+local msgText1 = display.newText( "Hello, world!", display.contentCenterX, display.contentWidth - 100, native.systemFont, 32 )
+msgText1:setFillColor( 0.1, 0.8, 0.7 )
+
+
+
+local msgText2 = display.newText( "Hello, world!", display.contentCenterX, display.contentWidth + 100, native.systemFont, 32 )
+msgText2:setFillColor( 0.7, 0.6, 0.7 )
 
